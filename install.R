7057a651ea69425fc374486b86a6d90e2704ca6e
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-02-01 15:32:43 +0100

    init

---
+++ install.R
@@ -0,0 +1,11 @@
+Sys.setenv(
+  C_INCLUDE_PATH=paste0(c(Sys.getenv("C_INCLUDE_PATH"), "/p/system/packages/proj/6.3.1/include"), collapse=":"),
+  CPLUS_INCLUDE_PATH=paste0(c(Sys.getenv("CPLUS_INCLUDE_PATH"), "/p/system/packages/proj/6.3.1/include"), collapse=":"),
+  LIBRARY_PATH=paste0(c(Sys.getenv("LIBRARY_PATH"), "/p/system/packages/proj/6.3.1/lib"), collapse=":"),
+  LD_LIBRARY_PATH=paste0(c(Sys.getenv("LD_LIBRARY_PATH"), "/p/system/packages/proj/6.3.1/lib"), collapse=":")
+)
+remotes::install_github("chroetz/ConfigOpts")
+remotes::install_github("chroetz/cerUtility")
+remotes::install_github("chroetz/cerExploreData")
+remotes::install_github("chroetz/cerProcessNetCdf")
+remotes::install_github("chroetz/cer")
---
+++ setCerEnvironmentVariables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export CER_ROOT="/p/tmp/cschoetz/cer"
+
+echo "CER_ROOT has been set to: $CER_ROOT"
